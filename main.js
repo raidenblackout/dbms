f529399f8782a3267649a2317620e76701b96e49
raidenblackout <xcdtlatin2@gmail.com>
2021-12-12 13:36:19 +0600

    minor fixes

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ window.onload = function(){
     const commentButtons = $('.post-reach .btn');
     commentButtons.on('click', function(e){
         const res=$(this).parents().filter('.post-comment-container').find('.comments');
-        console.log(res);
         res.toggleClass('hidden');
     });
 }
