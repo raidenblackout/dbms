ee77243179e4f6406b830f8ea48c9ccdb768fd79
raidenblackout <xcdtlatin2@gmail.com>
2021-12-12 12:42:45 +0600

    updated toggle comments

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-const commentButtons = document.getElementsByClassName("post-reach").getElementsByClassName("btn");
-function toggleComments(e){
-    //get the button
-    let button = e.target;
-    //get the post id
-    console.log(e);
-}
+
 window.onload = function(){
-    //for each comment button
-    commentButtons.forEach(function(button){
-        //add event listener
-        button.addEventListener("click", toggleComments);
+    const commentButtons = $('.post-reach .btn');
+    commentButtons.on('click', function(e){
+        const res=$(this).parents().filter('.post-comment-container').find('.comments');
+        console.log(res);
+        res.toggleClass('hidden');
     });
 }
 
